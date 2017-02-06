ee595ca43749df9510fa777b7bc2fd0fd4e8233b
aallai <alexandre.allaire@mail.mcgill.ca>
2017-02-05 17:18:54 -0800

    Fix tabs.

--- bitstream.h
+++ bitstream.h
@@ -9,25 +9,25 @@ namespace compression {
 struct bitstream
 {
 public:
-	bitstream();
+    bitstream();
 
-	void append(std::string const& bits);
-	void append(uint64_t bits, unsigned int width);
+    void append(std::string const& bits);
+    void append(uint64_t bits, unsigned int width);
 
-	std::string read_str(unsigned int width);
-	uint64_t read_word(unsigned int width);
+    std::string read_str(unsigned int width);
+    uint64_t read_word(unsigned int width);
 
-	void print();
+    void print();
     uint64_t size();
     bool has_unread_bits();
 
 private:
-	void append_bit(bool bit);
-	unsigned int read_bit();
+    void append_bit(bool bit);
+    unsigned int read_bit();
 
-	uint64_t write_head;
-	uint64_t read_head;
-	std::vector<uint8_t> buf;
+    uint64_t write_head;
+    uint64_t read_head;
+    std::vector<uint8_t> buf;
 };
 
 }
